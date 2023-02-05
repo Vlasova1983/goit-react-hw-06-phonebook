2a366dca577f74877baaad92e668baa32c05e3d3
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-06 00:32:10 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,18 @@
-import {useState,useEffect} from "react";
+import styles  from './App.module.css';
+
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-import styles  from './App.module.css';
 
-
-export const App  =()=> {
-  const [filter,setInFilter] = useState('');
-  const [contacts,setInContacts] = useState(()=>{
-    return JSON.parse(localStorage.getItem('contacts')) || [];
-  });  
-
-  useEffect(()=>{      
-    localStorage.setItem('contacts', JSON.stringify(contacts));          
-  },[contacts]); 
-
-  const isContactInState = ({ name }) =>
-  !!contacts.filter(({name: prevName}) => {return prevName === name}).length;
-
-  const handleSubmit = ({ name, number }) => { 
-    if (isContactInState({ name })) {
-      alert('Contact is in phonebook');
-      return;    
-    }   
-    setInContacts([...contacts, { id:getRandomID(), name, number}]);      
-  };
-
-  const handleFilterContacts =(value)=>{ 
-    setInFilter(value);       
-  };
-
-  const getFilterContact=()=> {    
-    return contacts.filter((contact)=>contact.name.toLowerCase().includes(filter));
-  };
-
-
-  const handleDeleteContact =(id)=>{   
-    setInContacts(contacts.filter((contact)=>contact.id !== id));  
-    return contacts;    
-  };
-
-  const getRandomID=()=> {
-    return `${Math.floor(Math.random() * 16777215).toString(16)}`;
-  };
-    
+export const App  =()=> {    
   return (
     <div className={styles.conteiner}>
       <h1>Phonebook</h1>
-      <ContactForm   onSubmit={handleSubmit}/>
+        <ContactForm/>
       <h2>Contacts</h2>
-      <Filter filter={filter} onFilter={handleFilterContacts}/>
-      <ContactList contacts={getFilterContact()} onDelete={handleDeleteContact}/>               
+        <Filter/>      
+        <ContactList/>               
     </div>
   );     
 };
